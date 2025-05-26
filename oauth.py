90d08f82be67fe51104f98356a1d2641c8f79361
Kyler2704 <kyler2704@gmail.com>
2025-05-25 22:04:01 -0400

    Update oauth.py

--- oauth.py
+++ oauth.py
@@ -42,7 +42,7 @@ def oauth2callback():
         'client_secret': creds.client_secret,
         'scopes': creds.scopes
     }
-    return redirect('/dashboard')
+    return redirect('https://guestmic.web.app/dashboard.html')
 
 @oauth_bp.route('/drive-status', methods=['GET'])
 def drive_status():
