01e76c4ac79e12b7d1c63f3f677d3111f023b78d
Kyler2704 <kyler2704@gmail.com>
2025-05-26 22:18:19 -0400

    Update guest.py

--- guest.py
+++ guest.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 guest_bp = Blueprint('guest_bp', __name__)
 
 
-@guest_bp.route('/guest/<slug>')
+@guest_bp.route('/<slug>')
 def serve_guest(slug):
     """Render the guest recorder page with the passed slug."""
     return render_template('GuestMicrecord.html', slug=slug)
